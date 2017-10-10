6daa7a7c5f5d113456023f4c16ff21a1de648c64
Naik <yogini.naik@capgemini.com>
2017-10-10 12:12:34 +0530

    caL MODIFIED

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,16 +1,16 @@
 
 public class Calculator {
 public static void subtract(int num1,int num2){
-	int res= num1+num2;
-	System.out.println("addition : "+ res);
+	int res= num1- num2;
+	System.out.println("subtraction : "+ res);
 }
 public static void add(int num1,int num2){
 	int res= num1+num2;
 	System.out.println("addition : "+ res);
 }
 public static void divide (int num1,int num2){
-	int res= num1+num2;
-	System.out.println("addition : "+ res);
+	double res= (double)num1/num2;
+	System.out.println(" div result : "+ res);
 }
 
 }
